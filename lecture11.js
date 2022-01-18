a1aba7c119b250bef95345c273b826ffee433b1c
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-18 18:26:19 +0900

    Delete lecture11.js

--- lecture11.js
+++
@@ -1,4 +0,0 @@
-2 * 3 > 4 + 5 && 6 / 3 == 2 || !false
-
-height >= 180 && gender == "male" || height >= 165 && gender == "female"
-(height >= 180 && gender == "male") || (height >= 165 && gender == "female")
