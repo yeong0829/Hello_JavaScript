cd04f6dfb40162f051bb164453522bc5d7d69d0e
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-18 13:53:01 +0900

    파트4. 연산자와 함수
    파트4. 연산자와 함수

---
+++ lecture11.js
@@ -0,0 +1,4 @@
+2 * 3 > 4 + 5 && 6 / 3 == 2 || !false
+
+height >= 180 && gender == "male" || height >= 165 && gender == "female"
+(height >= 180 && gender == "male") || (height >= 165 && gender == "female")
