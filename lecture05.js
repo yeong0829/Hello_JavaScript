d701e85c89df72752138466dd877816c3b768a39
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-18 13:49:35 +0900

    파트3. 자료형
    파트3. 자료형

---
+++ lecture05.js
@@ -0,0 +1,8 @@
+var height = prompt("키를 입력해 주세요");
+console.log(height, typeof(height));
+
+var height_ing = parseInt(height);
+console.log(height_ing, typeof(height_ing));
+
+var height_float = parseFloat(height);
+console.log(height_float, typeof(height_float));
